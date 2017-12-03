bf19b7fdca813b31977df3b684e6c4d565506915
EikaNN <octavi94@gmail.com>
2017-12-03 21:04:17 +0100

    Complete day 3

--- adventOfSolutions.py
+++ adventOfSolutions.py
@@ -1,16 +1,24 @@
 from day1.captcha import Captcha
 from day2.checksum import Checksum
+from day3.memory import Memory
 
 print('***************')
 print('**** Day 1 ****')
 print('***************')
 captcha = Captcha(open('./day1/input.txt').read())
-print('-    Part One: ' + str(captcha.solve_part_one()))
-print('-    Part Two: ' + str(captcha.solve_part_two()))
+print('- Part One: ' + str(captcha.solve_part_one()))
+print('- Part Two: ' + str(captcha.solve_part_two()))
 
 print('***************')
 print('**** Day 2 ****')
 print('***************')
 checksum = Checksum(open('./day2/input.txt').read())
-print('-    Part One: ' + str(checksum.solve_part_one()))
-print('-    Part Two: ' + str(checksum.solve_part_two()))
+print('- Part One: ' + str(checksum.solve_part_one()))
+print('- Part Two: ' + str(checksum.solve_part_two()))
+
+print('***************')
+print('**** Day 3 ****')
+print('***************')
+memory = Memory(open('./day3/input.txt').read())
+print('- Part One: ' + str(memory.solve_part_one()))
+print('- Part Two: ' + str(memory.solve_part_two()))
---
+++ day3/memory.py
@@ -0,0 +1,81 @@
+from itertools import cycle
+from collections import defaultdict
+
+class Square:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __hash__(self):
+        return hash( (self.x, self.y) )
+
+    def up(self):
+        return Square(self.x, self.y+1)
+
+    def down(self):
+        return Square(self.x, self.y-1)
+
+    def right(self):
+        return Square(self.x+1, self.y)
+
+    def left(self):
+        return Square(self.x-1, self.y)
+
+    def adjacents(self):
+        return [
+            self.up(), self.down(), self.right(), self.left(),
+            Square(self.x-1, self.y-1), Square(self.x-1, self.y+1),
+            Square(self.x+1, self.y-1), Square(self.x+1, self.y+1)
+        ]
+
+    def distance(self, square):
+        return abs(self.x - square.x) + abs(self.y - square.y)
+
+
+class Memory:
+
+    def __init__(self, target):
+        self.target = int(target)
+        self.directions = [Square.right, Square.up, Square.left, Square.down]
+        self.initial_square = Square(0, 0)
+
+    def solve_part_one(self):
+
+        current_square = self.initial_square
+        squares_to_move = 1
+        data = 1
+        moves = cycle(self.directions)
+
+        while True:
+            for _ in range(2):
+                move = next(moves)
+                for _ in range(squares_to_move):
+                    if data == self.target:
+                        return current_square.distance(self.initial_square)
+                    current_square = move(current_square)
+                    data += 1
+            squares_to_move += 1
+
+    def solve_part_two(self):
+
+        current_square = self.initial_square
+        squares_to_move = 1
+        moves = cycle(self.directions)
+        memory = defaultdict(int)
+        memory[current_square] = 1
+
+        while True:
+            for _ in range(2):
+                move = next(moves)
+                for _ in range(squares_to_move):
+                    if memory[current_square] > self.target:
+                        return memory[current_square]
+                    current_square = move(current_square)
+                    adjacent_squares = current_square.adjacents()
+                    data = sum(map(lambda x: memory[x], adjacent_squares))
+                    memory[current_square] = data
+            squares_to_move += 1
